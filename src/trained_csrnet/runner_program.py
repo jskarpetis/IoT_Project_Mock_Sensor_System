4c0dc0211269586dbbfaf541f77da7da64bb6260
John Skarpetis <johnskarpetis@gmail.com>
2023-01-22 21:11:21 +0200

    Various

--- src/trained_csrnet/runner_program.py
+++ src/trained_csrnet/runner_program.py
@@ -16,44 +16,27 @@ import json
 # Contains Camera Id - With maximum number of people in the area
 camera_base_set = {'camera-id-101392U': 100}
 
-# Creating the json to upload to the server.
-# def json_to_upload_generator():
-    
-    # camera_list = ['camera-id-101392U',
-    #                'camera-id-438593U', 'camera-id-472395U']
-    # density_list = [x for x in range(0, 100, 5)]
-
-    # data_dictionary = {}
-
-    # for camera in camera_list:
-    #     generated_data = {
-    #         "camera_id": camera,
-    #         "image_bytes": 'testing_bytes',
-    #         "density": str(numpy.random.choice(density_list))
-    #     }
-    #     data_dictionary[camera] = generated_data
-
-    # return data_dictionary
-
 
 async def image_handler(model, image_name):
-    
+
     # All of this is for one camera
     data_dictionary = {}
     model_prediction = make_prediction(model, image_to_predict=str(image_name))
     maximum_people_count = camera_base_set['camera-id-101392U']
-    
+
     data = {
         "camera_id": 'camera-id-101392U',
         "image_bytes": 'None',
         "density": numpy.round(float(model_prediction / maximum_people_count * 100), 1)
     }
-    
+
     data_dictionary['camera-id-101392U'] = data
-    
+
     await make_request(data_dictionary, 'camera-id-101392U')
 
 # Input is a dictionary with a specific key to upload
+
+
 async def make_request(generated_dictionary_data, key):
     endpoint_url = "https://hffi70fkl9.execute-api.eu-north-1.amazonaws.com/dev/upload-data-to-s3"
 
@@ -67,8 +50,7 @@ async def make_request(generated_dictionary_data, key):
 
 # Main Runner Task
 async def async_task_handler(model, task_seconds):
-    
-    
+
     image_name_list = list_images()
     image_counter = 0
     try:
@@ -91,9 +73,10 @@ def list_images():
             if character.isdigit():
                 image_number_identifier += character
         image_name_list[image] = int(image_number_identifier)
-            
-    sorted_dictionary = {key: val for key, val in sorted(image_name_list.items(), key=lambda element: element[1])}
-        
+
+    sorted_dictionary = {key: val for key, val in sorted(
+        image_name_list.items(), key=lambda element: element[1])}
+
     return list(sorted_dictionary.keys())
 
 
@@ -103,11 +86,11 @@ if __name__ == "__main__":
     event = asyncio.get_event_loop()
     try:
         print('Camera Task Started')
-        task_object_loop = event.create_task(async_task_handler(model, task_seconds=10))
+        task_object_loop = event.create_task(
+            async_task_handler(model, task_seconds=10))
         event.run_until_complete(task_object_loop)
     finally:
         event.close()
     print("Task status: {}".format(task_object_loop.result()))
-    
+
     # print(list_images())
-    
